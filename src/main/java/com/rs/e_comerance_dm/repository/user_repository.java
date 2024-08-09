9222bda1244ac62d883e44803229527e55d33a2a
Đinh Đức Đạt <91339985+DinhDat-1012@users.noreply.github.com>
2024-08-09 18:30:45 +0700

    debug_reinstall

--- src/main/java/com/rs/e_comerance_dm/repository/user_repository.java
+++
@@ -1,9 +0,0 @@
-package com.rs.e_comerance_dm.repository;
-
-import com.rs.e_comerance_dm.Model.user;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface user_repository extends JpaRepository<user,Long> {
-    user findByUsername(String userName);
-}
